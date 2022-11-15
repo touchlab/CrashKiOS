d3e7d3311e29a4f2197aa45f212a983769864226
Kevin Galligan <kgalligan@gmail.com>
2022-11-15 08:30:36 -0500

    Save again

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -26,31 +26,30 @@ const FeatureList: FeatureItem[] = [
 
 function Feature({title, url, Svg, description}: FeatureItem) {
   return (
-      <div className={clsx('col col--4')}>
-        <div className="text--center">
-          <Link to={url}>
-            <Svg className={styles.featureSvg} role="img" />
-          </Link>
-        </div>
-        <div className="text--center padding-horiz--md">
-          <h3>{title}</h3>
-          <p>{description}</p>
-        </div>
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Link to={url}>
+        <Svg className={styles.featureSvg} role="img" />
+        </Link>
       </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
   );
 }
 
-
 export default function HomepageFeatures(): JSX.Element {
   return (
-      <section className={styles.features}>
-        <div className="container">
-          <div className="row">
-            {FeatureList.map((props, idx) => (
-                <Feature key={idx} {...props} />
-            ))}
-          </div>
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
         </div>
-      </section>
+      </div>
+    </section>
   );
 }
