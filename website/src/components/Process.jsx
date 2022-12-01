2f7691944326ff29546f2a4688ab09e0c78bb247
Kevin Galligan <kgalligan@gmail.com>
2022-12-01 15:31:54 -0500

    All cyan

--- website/src/components/Process.jsx
+++ website/src/components/Process.jsx
@@ -25,9 +25,9 @@ function Process() {
             <div className="relative flex flex-col items-center" data-aos="fade-up">
               <div aria-hidden="true" className="absolute h-1 border-t border-dashed border-gray-700 hidden md:block" style={{ width: 'calc(100% - 32px)', left: 'calc(50% + 48px)', top: '32px' }} data-aos="fade-in" data-aos-delay="200"></div>
               <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <rect className="fill-current text-lime-600" width="64" height="64" rx="32" />
-                <path className="stroke-current text-lime-300" strokeWidth="2" strokeLinecap="square" d="M21 23h22v18H21z" fill="none" fillRule="evenodd" />
-                <path className="stroke-current text-lime-100" d="M26 28h12M26 32h12M26 36h5" strokeWidth="2" strokeLinecap="square" />
+                <rect className="fill-current text-cyan-600" width="64" height="64" rx="32" />
+                <path className="stroke-current text-cyan-300" strokeWidth="2" strokeLinecap="square" d="M21 23h22v18H21z" fill="none" fillRule="evenodd" />
+                <path className="stroke-current text-cyan-100" d="M26 28h12M26 32h12M26 36h5" strokeWidth="2" strokeLinecap="square" />
               </svg>
               <h4 className="h4 mb-2"><span className="text-gray-400">1</span>. Symbolication</h4>
               <p className="text-lg text-gray-400 text-center">iOS crash handling is different between Swift and Kotlin. CrashKiOS properly symbolicates Kotlin stack traces.</p>
@@ -37,10 +37,10 @@ function Process() {
             <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="200">
               <div aria-hidden="true" className="absolute h-1 border-t border-dashed border-gray-700 hidden md:block" style={{ width: 'calc(100% - 32px)', left: 'calc(50% + 48px)', top: '32px' }} data-aos="fade-in" data-aos-delay="400"></div>
               <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <rect className="fill-current text-lime-600" width="64" height="64" rx="32" />
+                <rect className="fill-current text-cyan-600" width="64" height="64" rx="32" />
                 <g fill="none" fillRule="evenodd">
-                  <path className="stroke-current text-lime-300" d="M40 22a2 2 0 012 2v16a2 2 0 01-2 2H24a2 2 0 01-2-2V24a2 2 0 012-2" strokeWidth="2" strokeLinecap="square" />
-                  <path className="stroke-current text-lime-100" strokeWidth="2" strokeLinecap="square" d="M36 32l-4-3-4 3V22h8z" />
+                  <path className="stroke-current text-cyan-300" d="M40 22a2 2 0 012 2v16a2 2 0 01-2 2H24a2 2 0 01-2-2V24a2 2 0 012-2" strokeWidth="2" strokeLinecap="square" />
+                  <path className="stroke-current text-cyan-100" strokeWidth="2" strokeLinecap="square" d="M36 32l-4-3-4 3V22h8z" />
                 </g>
               </svg>
               <h4 className="h4 mb-2"><span className="text-gray-400">2</span>. Metadata</h4>
@@ -50,9 +50,9 @@ function Process() {
             {/* 3rd item */}
             <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="400">
               <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                <rect className="fill-current text-lime-600" width="64" height="64" rx="32" />
-                <path className="stroke-current text-lime-300" strokeWidth="2" strokeLinecap="square" d="M21 35l4 4 12-15" fill="none" fillRule="evenodd" />
-                <path className="stroke-current text-lime-100" d="M42 29h-3M42 34h-7M42 39H31" strokeWidth="2" strokeLinecap="square" />
+                <rect className="fill-current text-cyan-600" width="64" height="64" rx="32" />
+                <path className="stroke-current text-cyan-300" strokeWidth="2" strokeLinecap="square" d="M21 35l4 4 12-15" fill="none" fillRule="evenodd" />
+                <path className="stroke-current text-cyan-100" d="M42 29h-3M42 34h-7M42 39H31" strokeWidth="2" strokeLinecap="square" />
               </svg>
               <h4 className="h4 mb-2"><span className="text-gray-400">3</span>. Fatal Crashes</h4>
               <p className="text-lg text-gray-400 text-center">Captures fatal crashes as well as handled exception reports.</p>
