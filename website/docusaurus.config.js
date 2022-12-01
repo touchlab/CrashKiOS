ce225aec3b77cd962a09d277ef393d0710f534b1
Kevin Galligan <kgalligan@gmail.com>
2022-12-01 09:39:33 -0500

    Site update

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -151,6 +151,17 @@ const config = {
 
   // plugins: ['@docusaurus/plugin-content-tldocs'],
   plugins: [
+    async function myPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
     [
       '@docusaurus/plugin-google-gtag',
       {
--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Process from '@site/src/components/Process';
+import HeroHome from '@site/src/components/HeroHome';
+import Newsletter from '@site/src/components/Newsletter';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
 
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
@@ -24,10 +12,11 @@ export default function Home(): JSX.Element {
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      <HeroHome />
+      <Process/>
+      <Newsletter/>
+      <div>&nbsp;</div>
+      <div>&nbsp;</div>
     </Layout>
   );
 }
