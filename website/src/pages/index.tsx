1e82765989ce75d87226ee036224734f6e198e87
Kevin Galligan <kgalligan@gmail.com>
2022-12-01 20:30:46 -0500

    Animate on scroll

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Process from '@site/src/components/Process';
 import HeroHome from '@site/src/components/HeroHome';
 import Newsletter from '@site/src/components/Newsletter';
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
+
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: 'phone',
+      duration: 350,
+      easing: 'ease-out-sine',
+    });
+  });
+
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
